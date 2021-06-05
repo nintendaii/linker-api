7a413a8681209b09fed43f74143147384b15b0e7
Daniel <danielkarshai8@gmail.com>
2021-06-05 18:57:03 +0300

    cors fix

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('config')
-const bodyParser = require('body-parser')
-const routes = require('./routes');
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const cors = require("cors");
 
-const app = express()
+const app = express();
+//cors({ origin: "http://localhost:8080" });
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(routes)
-
-const PORT = config.get('port')
+const PORT = config.get("port");
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoURI'), {
+    await mongoose.connect(config.get("mongoURI"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true
+      useCreateIndex: true,
     });
     app.listen(PORT, () => console.log(`App is started on port ${PORT}`));
   } catch (error) {
-    console.log('error'+error);
+    console.log("error" + error);
     process.exit(1);
   }
 }
--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -1,47 +1,56 @@
-const {Router} = require('express')
-const router = Router()
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken');
-const {check,validationResult} = require('express-validator')
-const User = require('../../../models/User')
-const userController = require('../../../controllers').userController
-
-router.post('/signup',
-  [
-    check('email', 'Invalid email').isEmail(),
-    check('password', 'Invalid passwod (min 6 chars)').isLength({ min: 6 })
-  ],
-  async(req,res)=>{
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array(), message: 'Invalid data' });
-      }
-    let user = await userController.signup.signup(req.body);
-    res.status(user.status).json(user)
-  } catch (error) {
-    res.status(400).json({message:"Something went wong ("+error})
-  }
-})
+const { Router } = require("express");
+const router = Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const User = require("../../../models/User");
+const userController = require("../../../controllers").userController;
 
 router.post(
-  '/login',
+  "/signup",
   [
-    check('email', 'Invalid email').normalizeEmail().isEmail(),
-    check('password', 'Invalid passwod (min 6 chars)').exists()
+    check("email", "Invalid email").isEmail(),
+    check("password", "Invalid passwod (min 6 chars)").isLength({ min: 6 }),
   ],
   async (req, res) => {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array(), message: 'Invalid data' });
+        return res
+          .status(400)
+          .send({ errors: errors.array(), message: "Invalid data" });
       }
-      let userModel = await userController.login.login(req.body)
-      res.status(userModel.status).json(userModel)
+      let user = await userController.signup.signup(req.body);
+      res.status(user.status).json(user);
     } catch (error) {
-      res.status(500).json({ message: 'Something went wrong :(' });
+      res.status(400).json({ message: "Something went wong (" + error });
     }
   }
-)
+);
 
-module.exports=router
+router.post(
+  "/login",
+  [
+    check("email", "Invalid email").normalizeEmail().isEmail(),
+    check("password", "Invalid passwod (min 6 chars)").exists(),
+  ],
+  async (req, res) => {
+    try {
+      console.log(req.body);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log("sss");
+        console.log(errors.array());
+        return res
+          .status(400)
+          .send({ errors: errors.array(), message: "Invalid data" });
+      }
+      let userModel = await userController.login.login(req.body);
+      res.status(userModel.status).send(userModel);
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong :(" });
+    }
+  }
+);
+
+module.exports = router;
