2380474d9a88312d852c2410cb0051fa3af1e9b4
Daniel <danielkarshai8@gmail.com>
2021-06-19 00:33:40 +0300

    port fix

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(routes);
 
-const PORT = config.get("port");
+const PORT = config.get("port") || process.env.PORT;
 
 async function start() {
   try {
