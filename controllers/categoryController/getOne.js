1e3d1a219be0f5998bfd6e6fee4ac045a4b67afb
Daniel <danielkarshai8@gmail.com>
2021-07-02 18:36:02 +0300

    get one category links fix

--- controllers/categoryController/getOne.js
+++ controllers/categoryController/getOne.js
@@ -1,12 +1,15 @@
+const Bookmark = require("../../models/Bookmark");
 const Category = require("../../models/Category");
 
 async function getOne(id) {
   try {
-    const category = await Category.findOne({ _id: id });
+    let category = await Category.findOne({ _id: id });
     console.log(category);
     if (!category) {
       return { status: 404, code: "category_doesnt_exist" };
     }
+    let linksObjs = await Bookmark.find({ _id: category.links });
+    category.links = linksObjs;
     return { status: 200, data: category };
   } catch (error) {
     return { status: 400, code: "Something went wong ( " + error };
